3aac90d731b821d5bcea369a590b529cdfe47d5f
OMJS2769 <mauricio2769@outlook.es>
2025-01-10 13:26:20 -0600

    footer copyright

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -15,14 +15,14 @@
         @if (request()->session()->get('cliente', 'default_value') != 'flutter')
             <div class="col-md-3">
                 <h6><strong>SÍGUENOS EN</strong></h6>
-                <a href="https://www.facebook.com/profile.php?id=100066569195961&mibextid=ZbWKwL" target="_blank"
-                    class="btn btn-primary" title="Faceboox">
+                <a href="https://www.facebook.com/search/top?q=katzesystems.inc" target="_blank" class="btn btn-primary"
+                    title="Faceboox">
                     <span class="icon icon-facebook"></span>
                 </a>
-                &nbsp;&nbsp;&nbsp;
+                {{--  &nbsp;&nbsp;&nbsp;
                 <div class="fb-like" data-href="https://www.facebook.com/profile.php?id=100066751009079&locale=es_LA"
                     data-width="" data-layout="button_count" data-action="like" data-size="small" data-share="false">
-                </div>
+                </div>  --}}
             </div>
         @endif
         <div class="col-md-3">
@@ -35,6 +35,14 @@
                 Teléfono: 55 55799356<br>
                 WhatsApp: 55 39799471<br>
             </small>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-md-12 text-center">
+            | Copyright © {{ date('2025') }} |
+            <a href="https://www.facebook.com/search/top?q=katzesystems.inc"><img
+                    src="{{ asset('img/logo_katze.png') }}" width="20" height="20"> KatzeSystems.Inc</a>
+            | Todos los derechos reservados. |
         </div>
     </div>
 </div>
